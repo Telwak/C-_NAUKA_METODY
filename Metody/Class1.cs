1f0bbadfc5f283146b96a046b04e48709578f0d1
Telwak <tomasz.telwak@telwak.pl>
2017-06-29 14:03:09 +0200

    Dodanie

---
+++ Metody/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody
+{
+    //Definiujemy klase publiczną.
+    public class Gdzie_mieszkałeś
+    {
+        //Publiczna metoda wirtualna
+        public virtual string Miasto() { return "Przemyśl"; }
+    }
+    //Publiczna klasa która dziedziczy po klasie "Gdzie_mieszkałeś"
+    public class Gdzie_mieszkasz_obecnie : Gdzie_mieszkałeś
+    {
+        //Definiujemy metodę "override" która nadpisuje metodę wyżej.
+        public override string Miasto() { return "Wrocław"; }
+    }
+
+    //Definiujemy publiczną klasę
+    public class Samochód
+    {
+        //Publiczna wirtualna metoda
+        public virtual string Marka() { return "Ferrari"; }
+
+    }
+    //Publiczna klasa dziedzicząca po "Samochód"
+    public class Samochód2 : Samochód
+    {
+        //Definiujemy metodę "new" która dodaje kolejną metodę (Nie nadpisuje istniejącej).
+        new public string Marka() { return "Porshe"; }
+    }
+}
---
+++ Metody/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+namespace Metody
+{
+    //Definiujemy interfejs
+    interface Zawody
+    {
+        void Tomasz_Telwak();
+    }
+    //Definiujemy klasę abstrakcyjną
+    public abstract class Wiek
+    {
+        //Definiujemy metodę wirtualną
+        public virtual void Mój_wiek(int Wiek) { Console.WriteLine("Wiek: "+Wiek); Console.ReadKey(); }
+    }
+    class Metody : Wiek
+    {
+        static void Main(string[] args)
+        {
+            //Metoda Statyczna (Nie trzeba tworzyć obiektu)
+            Jak_masz_na_imie("Tomasz");
+            //Tworzenie obiektu
+            Metody Glowna = new Metody();
+            //Metoda bez modyfikatorów (Musimu utworzyć obiekt).
+            Glowna.Jak_masz_na_nazwisko("Telwak");
+            //Klasa abstrakcyjna odwołanie do metody wirtualnej
+            Glowna.Mój_wiek(23);
+
+            //Tworzenie obiektu (Ludzie)
+            Ludzie Zawody = new Ludzie();
+            //interfejs
+            Zawody.Tomasz_Telwak();
+            //Tworzymy obiekty
+            Gdzie_mieszkałeś Gdzie_mieszkałeś = new Gdzie_mieszkałeś();
+            Gdzie_mieszkasz_obecnie Gdzie_mieszkasz = new Gdzie_mieszkasz_obecnie();
+            Samochód Wymarzony = new Samochód();
+            Samochód2 Wymarzony2 = new Samochód2();
+            //Wyświetlamy rezultat
+            Console.WriteLine("Mieszkałem w: " + Gdzie_mieszkałeś.Miasto());
+            Console.WriteLine("Mieszkam w: " + Gdzie_mieszkasz.Miasto());
+            Console.ReadKey();
+            Console.WriteLine("Pierwszy wymarzony samochód: " + Wymarzony.Marka());
+            Console.WriteLine("Drugi wymarzony samochód: " + Wymarzony2.Marka());
+            Console.ReadKey();
+
+
+        }
+
+
+
+        //Metoda statyczna może być wywoływania bez konieczności tworzenia obiektu.
+        public static String Jak_masz_na_imie(string Imie)
+        {
+            Console.WriteLine("Mam na imię: "+Imie);
+            Console.ReadKey();
+            return Imie;
+        }
+        //Metoda bez modyfikatorów.
+        public string Jak_masz_na_nazwisko(string Nazwisko)
+        {
+            Console.WriteLine("Mam na Nazwisko: " + Nazwisko);
+            Console.ReadKey();
+            return Nazwisko;
+        }
+
+    }
+    //Nowa klasa Dziedzicząca interfejs (Metody w interfejsie muszą być wykorzystywane)
+    class Ludzie : Zawody
+    {
+        public void Tomasz_Telwak()
+        {
+            Console.WriteLine("Mój zawód to: Programista");
+            Console.ReadKey();
+        }
+    }
+    
+}
